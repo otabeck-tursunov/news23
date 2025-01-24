f3887343c4b6b15afcc13899ccc105243f4c626d
Otabek Tursunov <otabecktursunov@gmail.com>
2025-01-24 19:19:47 +0500

    static setting conf

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = 'static/'
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
